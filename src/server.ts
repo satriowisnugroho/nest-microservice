6cda75b4dfef3cdb06b79b79e5b28f184d073d70
Satrio Wisnugroho <wisnu@skyshi.com>
2018-01-16 08:57:29 +0700

    Add port configuration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import * as dotenv from 'dotenv';
 import * as Raven from 'raven';
+import * as argv from 'minimist';
+import { random } from 'lodash';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 
 dotenv.config();
 Raven.config(process.env.SENTRY_DSN).install();
+const arg = argv(process.argv.slice(2));
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(ApplicationModule, {
-    port: process.env.APP_PORT,
-  });
-  app.listen(() => console.log(`User Microservice is listening on port ${process.env.APP_PORT}`));
+  const port = arg.port || random(50000, 65000);
+  const app = await NestFactory.createMicroservice(ApplicationModule, { port });
+  app.listen(() => console.log(`User Microservice is listening on port ${port}`));
 }
 bootstrap();
